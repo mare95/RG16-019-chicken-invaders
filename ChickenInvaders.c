ff8e3fc9a5ce5bc59ca300ffc52482b8a501fadb
mare95 <petricicmarko1995@gmail.com>
2016-12-09 20:11:47 +0100

    naprevljene kokoske i odradjeno njihovo kretanje nezavisno od aviona

--- ChickenInvaders.c
+++ ChickenInvaders.c
@@ -3,28 +3,40 @@
 #include <time.h>
 #include <stdlib.h>
 
-#define TIMER_ID 0
-#define TIMER_INTERVAL 10
+#define TIMER_ID1 0
+#define TIMER_INTERVAL1 10
+#define TIMER_ID2 1
+#define TIMER_INTERVAL2 10
 
 #define GORE_MAX 6
 #define DOLE_MAX 0
 #define LEVO_MAX -20.1
 #define DESNO_MAX 20.1
-#define SPEED 0.6 /*brzina kojom se krece avion*/
+#define SPEED 0.2 /*brzina kojom se krece avion*/
+
 static int window_width;
 static int window_height;
+
+static float v_x=0.2; /*brzina kokoske po x*/
 
 static float x_curr, y_curr;    /* Tekuce koordinate aviona. */
 static int napred=0;/*samo da nije 1 ili -1*/
 static int levo=0;/*flegovi smera kretanja aviona*/
+
+static float cx_curr, cy_curr;	/* Tekuce koordinate kokoske*/
+static int c_napred=0;/*samo da nije 1 ili -1*/
+static int c_levo=0;/*flegovi smera kretanja aviona*/
+
 static int animation_ongoing;   /* Fleg koji odredjuje da li je
                                  * animacija u toku. */
 
-
+void drawChicken();
+void drawPlane();
 static void on_display(void);
 static void on_keyboard(unsigned char key, int x, int y);
 static void on_reshape(int width, int height);
-static void on_timer(int value);
+static void on_timer1(int value);
+static void on_timer2(int value);
 
 int main(int argc, char **argv){
 	glutInit(&argc, argv);
@@ -41,6 +53,8 @@ int main(int argc, char **argv){
 
     	x_curr = 0;
     	y_curr = 0;
+	cx_curr=0;
+	cy_curr=0;
 
     	animation_ongoing = 0;
 
@@ -78,11 +92,15 @@ static void on_keyboard(unsigned char key, int x, int y){
    		case 'G':
     		    /* Pokrecemo igru*/
       		  if (animation_ongoing==0) {
-        		glutTimerFunc(TIMER_INTERVAL, on_timer, TIMER_ID);
+        		glutTimerFunc(TIMER_INTERVAL1, on_timer1, TIMER_ID1);
+			glutTimerFunc(TIMER_INTERVAL2, on_timer2, TIMER_ID2);
         		animation_ongoing = 1;
 			}
       		  break;
-
+		case 'Q':
+		case 'q':
+			glutFullScreen();
+			break;
     		case 'z':
     		case 'Z':
         		/* Zaustavljamo igru. */
@@ -110,9 +128,32 @@ static void on_display(void){
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 	gluLookAt(0, -2, 10, 0, 0, 0, 0, 1, 0);
-	
-	/*avion*/
+
 	glTranslatef(x_curr, y_curr, 0);
+	drawPlane();
+	glTranslatef(-x_curr, -y_curr, 0);/*ponistavamo transformaciju da ne utice na kokoske*/
+
+	/*iscrtavanje 3 reda kokoski*/
+	glTranslatef(cx_curr, 10+cy_curr, 0);
+	for(int i=0; i<10;i++){
+		glTranslatef(4, 0, 0);
+		drawChicken();
+	}
+	glTranslatef(-40, -4, 0);
+	for(int i=0; i<10;i++){
+		glTranslatef(4, 0, 0);
+		drawChicken();
+	}
+	glTranslatef(-40, -4, 0);
+	for(int i=0; i<10;i++){
+		glTranslatef(4, 0, 0);
+		drawChicken();
+	}
+	glutSwapBuffers();
+}
+
+/*avion*/
+void drawPlane(){
 	glPushMatrix();
 		/*prednje krilo*/
 		glColor3f(0, 0, 1);
@@ -138,14 +179,15 @@ static void on_display(void){
 		glTranslatef(0, -1, 0);
 		glutWireCube(1);
 	glPopMatrix();
+}
 
-	/*kokoska*/
-	glTranslatef(-x_curr, -y_curr, 0);
+/*kokoska*/
+void drawChicken(){
 	glPushMatrix();
 		/*telo*/
 		glColor3f(1, 0, 0);
-		glTranslatef(0, 1, 0);
-		glScalef(1, 2, 1);
+		glTranslatef(-22, 10, 0);
+		glScalef(0.5, 1, 0.5);
 		glutWireSphere(1, 10, 10);
 		/*glava*/
 		glColor3f(1, 0, 0);
@@ -179,61 +221,56 @@ static void on_display(void){
 		glTranslatef(-1.1, 0.2, 0);
 		glutWireCube(1);
 
-
 		glColor3f(1, 0, 0);
 		glScalef(1, 1, 1);
 		glTranslatef(-3.5, 0, 0);
 		glutWireCube(1);
 		
 	glPopMatrix();
-	glutSwapBuffers();
 }
-
-
-static void on_timer(int value){
+static void on_timer1(int value){
     /*proverava se da li poziv dolazi od odgovarajuceg tajmera*/
-    if (value != TIMER_ID)
+    if (value != TIMER_ID1)
         return;
 
 /*cuvamo kretanje*/
 int n = napred;
 int l = levo;
 
-
 levo=0;/*levo i desno gasimo*/
 
     if(napred==1){
-		y_curr += SPEED; /*brzina po y-osi*/
-		/*printf("%d, %f\n", window_height, y_curr);*/
-    		if (y_curr >= GORE_MAX) {/*najdalja tacka do koje avion sme da ide napred*/
-        		y_curr = GORE_MAX;/*zakucavamo za ivicu ako dodje do gornje granice*/
-    		}
-	}
-    else if(napred==-1){ /*brzina po yosi*/
-		/*printf("%d, %f\n", window_height, y_curr);*/
-		y_curr -= SPEED;
-		if (y_curr <= DOLE_MAX) {/*najdalja tacka do koje avion sme da ide nazad*/
-        		y_curr =DOLE_MAX;/*zakucavamo za ivicu ako udari dole*/
-    		}
+	y_curr += SPEED; /*brzina po y-osi*/
+	/*printf("%d, %f\n", window_height, y_curr);*/
+    	if (y_curr >= GORE_MAX) {/*najdalja tacka do koje avion sme da ide napred*/
+            y_curr = GORE_MAX;/*zakucavamo za ivicu ako dodje do gornje granice*/
+    	}
     }
-	napred=0;/*gore i dole gasimo*/
-	levo=l;/*vracamo levo i desno*/
+    else if(napred==-1){ /*brzina po yosi*/
+	/*printf("%d, %f\n", window_height, y_curr);*/
+	y_curr -= SPEED;
+	if (y_curr <= DOLE_MAX) {/*najdalja tacka do koje avion sme da ide nazad*/
+            y_curr =DOLE_MAX;/*zakucavamo za ivicu ako udari dole*/
+    	}
+    }
+	
+napred=0;/*gore i dole gasimo*/
+levo=l;/*vracamo levo i desno*/
 
     if(levo==1){
-		x_curr -= SPEED; /*brzina po xosi*/
-		if (x_curr <= LEVO_MAX) {/*najdalja tacka do koje avion sme da ide levo*/
-        		x_curr = LEVO_MAX; /*zakucavamo za ivicu ako udari levo*/
-    		}
+	x_curr -= SPEED; /*brzina po xosi*/
+        if (x_curr <= LEVO_MAX) {/*najdalja tacka do koje avion sme da ide levo*/
+            x_curr = LEVO_MAX; /*zakucavamo za ivicu ako udari levo*/
+        }
     }	
     else if(levo==-1){
-		/*printf("%d, %f\n", window_width, x_curr);*/
-		x_curr += SPEED;
-    		if (x_curr >= DESNO_MAX) { /*najdalja tacka do koje avion sme da ide desno*/
-        		x_curr = DESNO_MAX; /*zakucavamo za ivicu ako udari desno*/
-    		}
+	/*printf("%d, %f\n", window_width, x_curr);*/
+	x_curr += SPEED;
+        if (x_curr >= DESNO_MAX) { /*najdalja tacka do koje avion sme da ide desno*/
+            x_curr = DESNO_MAX; /*zakucavamo za ivicu ako udari desno*/
+        }
     }
    
-
 	levo=0;/*ni levo ni desno*/
 	napred=0;/*ni gore ni dole*/
 
@@ -242,6 +279,38 @@ levo=0;/*levo i desno gasimo*/
 
     /* Po potrebi se ponovo postavlja tajmer. */
     if (animation_ongoing) {
-        glutTimerFunc(TIMER_INTERVAL, on_timer, TIMER_ID);
+        glutTimerFunc(TIMER_INTERVAL1, on_timer1, TIMER_ID1);
     }
+}
+
+static void on_timer2(int value){
+    /*proverava se da li poziv dolazi od odgovarajuceg tajmera*/
+	if (value != TIMER_ID2)
+		return;
+
+	cx_curr += v_x;
+	if (cx_curr <= -15){ /*leva granica*/
+		cy_curr=cy_curr-0.1; 
+		v_x *= -1; /*menjamo smer kretanja*/
+	}
+	else if(cx_curr <= -14){
+		cy_curr=cy_curr-0.1;
+	}
+    	if (cx_curr >= 15){ /*desna granica*/
+		cy_curr=cy_curr-0.1;
+        	v_x *= -1;
+	}
+	else if (cx_curr >= 14){
+		cy_curr=cy_curr-0.1;
+	}
+
+   
+
+    /* Forsira se ponovno iscrtavanje prozora. */
+    glutPostRedisplay();
+
+    /* Po potrebi se ponovo postavlja tajmer. */
+	if (animation_ongoing) {
+		glutTimerFunc(TIMER_INTERVAL2, on_timer2, TIMER_ID2);
+	}
 }
