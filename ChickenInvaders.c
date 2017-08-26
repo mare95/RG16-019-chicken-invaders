7633dde59aa1b7a54fbb024c7c74bc48cf369a08
mare95 <petricicmarko1995@gmail.com>
2017-08-26 21:49:55 +0200

    implementirane sve kolizije (igra je kompletna)

--- ChickenInvaders.c
+++ ChickenInvaders.c
@@ -5,17 +5,17 @@
 #include <stdbool.h>
 #include <unistd.h>
 #define TIMER_ID1 0
-#define TIMER_INTERVAL1 50
+#define TIMER_INTERVAL1 35
 #define TIMER_ID2 1
-#define TIMER_INTERVAL2 50
+#define TIMER_INTERVAL2 35
 #define TIMER_ID3 2
-#define TIMER_INTERVAL3 50
+#define TIMER_INTERVAL3 35
 
 /*pocetak ogranicenja u kretanju aviona*/
 #define GORE_MAX 6 
 #define DOLE_MAX 0
 #define LEVO_MAX -20.1
-#define DESNO_MAX 20.1 
+#define DESNO_MAX 20.1
 /*kraj ogranicenja u kretanju aviona*/
 
 /*pocetak ogranicenja u kretanju kokoske*/
@@ -24,10 +24,12 @@
 /*kraj ogranicenja u kretanju kokoske*/
 
 #define MAX_BULLET_NUMBER 100
-#define SPEED 0.5 /*brzina kojom se krece avion*/
+/*brzina kojom se krece avion*/
+#define SPEED 0.7
 #define BULLET_SPEED 3
-#define BOSS_CHICKEN_STRENGTH 1000
-
+/*broj metaka koji je potreban za ubijanje boss kokoske*/
+#define BOSS_CHICKEN_STRENGTH 100
+#define EGG_MAX_Y 30
 
 /*komponente za avion*/
 GLfloat ambient_tp[] 	= {0.6, 0.2, 0.2, 1}; /*tamno plavo*/
@@ -53,46 +55,50 @@ static float bullet_ypos[MAX_BULLET_NUMBER]; /*y koordinata metka*/
 static int window_width;
 static int window_height;
 
-static float v_x=0.4; /*brzina kokoske po x*/
-static float BOSS_CHICKEN_SPEED = 0.1;
-static float x_curr, y_curr;    /* Tekuce koordinate aviona. */
-
 /*flegovi smera kretanja aviona pocetak*/
 static int napred=0;/*samo da nije 1 ili -1*/
 static int levo=0;
 /*flegovi smera kretanja aviona kraj*/
 
+static float x_curr, y_curr;    /* Tekuce koordinate aviona. */
 static float cx_curr, cy_curr;	/* Tekuce koordinate kokoske (chicken x, chicken y))*/
 static float bcx_curr, bcy_curr; /* Tekuce koordinate boss kokoske (big chicken x, big chicken y)*/
-
+static float eggx_curr, eggy_curr; /* Tekuce koordinate jajeta kokoske*/
 static float bex_curr, bey_curr; /*Tekuce koordinate velikog jajeta (big egg x, big egg y)*/
-bool lvl2; /*indikator za iscrtavanje drugog nivoa*/
 
-static int bossChickenHealth;
+static int bossChickenHealth; /*Holiko metaka moze istrpeti boss kokoska*/
+static float BOSS_CHICKEN_SPEED = 0.1;
 
+int numOfDeathChickens;
+static float v_x=0.4; /*brzina kokoske po x*/
+bool lvl2; /*Indikator za iscrtavanje drugog nivoa*/
 static int animation_ongoing;   /* Fleg koji odredjuje da li je
                                  * animacija u toku. */
-//void drawChicken();
 void drawPlane();
 void drawPlaneColider();
 void drawBullet();
 void drawBulletColider();
 void drawChicken();
 void drawChickenColider();
+void drawChickenEgg();
+void drawChickenEggColider();
 void drawBossChicken();
 void drawBossChickenColider();
 void drawBossChickenEgg();
 void drawBossChickenEggColider();
+
 static void on_display(void);
 static void on_keyboard(unsigned char key, int x, int y);
 static void on_reshape(int width, int height);
+
 static void on_timer1(int value);
 static void on_timer2(int value);
 static void on_timer3(int value);
+
 void kolizija_avion_kokoska();
 void kolizija_avion_boss_kokoska();
 void kolizija_avion_boss_jaja();
-int numOfDeathChickens;
+void kolizija_jaje_avion();
 
 void inicijalizacija_osvetljenja();
 void init();
@@ -104,7 +110,14 @@ typedef struct{
 	bool alive;
 }Kokoska;
 
+typedef struct{
+	double x;
+	double y;
+	bool go;
+}Jaje;
+
 Kokoska kokoske1[10], kokoske2[10], kokoske3[10];
+Jaje jaje1[10];
 
 int main(int argc, char **argv){
 	
@@ -137,6 +150,9 @@ void init(){
 	bcy_curr=0;
 	bex_curr=0;
 	bey_curr=0;
+	eggx_curr=0;
+	eggy_curr=0;
+	
 	/*inicijalizacija pozicija za metke*/
         for (int i = 0; i < MAX_BULLET_NUMBER; i++) {
             bullet_xpos[i] = 0;
@@ -157,6 +173,9 @@ void init(){
 		kokoske3[i].x = 0;
 		kokoske3[i].y = 0;
 		kokoske3[i].alive=true;
+		jaje1[i].x=0;
+		jaje1[i].y=0;
+		jaje1[i].go=false;
 	}
 
     	animation_ongoing = 0;
@@ -169,6 +188,7 @@ void init(){
 }
 
 static void on_keyboard(unsigned char key, int x, int y){
+
 	switch(key){
 		case 27:
 			exit(1);
@@ -228,11 +248,13 @@ static void on_keyboard(unsigned char key, int x, int y){
 	}
 
 static void on_reshape(int width, int height){
+
 	window_width=width;
 	window_height=height;
 }
 
 static void on_display(void){
+
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	/*podesavamo resize*/
 	glViewport(0, 0, window_width, window_height);
@@ -247,44 +269,57 @@ static void on_display(void){
 	glLoadIdentity();
 	gluLookAt(0, -2, 10, 0, 0, 0, 0, 1, 0);
 
-	glTranslatef(x_curr, y_curr, 0);
-	drawPlane();
-	drawBullet();/*nije najsrecnije resenje jer su metak i avion u fazi, mora fix*/
-	//drawBulletColider();
-	glTranslatef(-x_curr, -y_curr, 0);/*ponistavamo transformaciju da ne utice na kokoske*/
-
-
+	glPushMatrix();
+		glTranslatef(x_curr, y_curr, 0);
+		drawPlane();
+		drawBullet();/*nije najsrecnije resenje jer su metak i avion u fazi, mora fix*/
+		//drawBulletColider();
+	glPopMatrix();
 	//drawPlaneColider();
-
 
 	/*iscrtavanje 3 reda kokoski*/
 	glPushMatrix();
-	glTranslatef(cx_curr, 10+cy_curr, 0);
-	for(int i=0; i<10;i++){
-		glTranslatef(4, 0, 0);
-		if(kokoske1[i].alive)
-			drawChicken();
-		kokoske1[i].x=(cx_curr+4*i-19.6);
-		kokoske1[i].y=cy_curr+20.5;
-	}
-
-	//crtanje ostalih kokosaka
-	/*
-	glTranslatef(-40, -4, 0);
-	for(int i=0; i<10;i++){s
-		glTranslatef(4, 0, 0);
-		drawChicken();
-	}
-	glTranslatef(-40, -4, 0);
-	for(int i=0; i<10;i++){
-		glTranslatef(4, 0, 0);
-		drawChicken();
-	}
-	glTranslatef(-40, -12, 0);
-	*/
+		glTranslatef(cx_curr, 10+cy_curr, 0);
+		for(int i=0; i<10;i++){
+			glTranslatef(4, 0, 0);
+			if(kokoske1[i].alive){
+				drawChicken();
+				glPushMatrix();
+					if(jaje1[i].go){
+						glTranslatef(eggx_curr-22-cx_curr, -eggy_curr+11, 0);
+						drawChickenEgg();
+					}
+				glPopMatrix();
+			}
+			kokoske1[i].x=(cx_curr+4*i-19.6);
+			kokoske1[i].y=cy_curr+20.5;
+			jaje1[i].x=kokoske1[i].x+1-cx_curr + eggx_curr;
+			jaje1[i].y=kokoske1[i].y-1-eggy_curr;
+			if(eggy_curr==0 && rand() % 10 ==i)//ako je jaje na pocetnoj poziciji moze da krene
+				jaje1[i].go=true;
+			else if(eggy_curr==30)//ako je jaje na zavrsnoj poziciji ne moze da krene odmah
+				jaje1[i].go=false;
+		}
+		
+		glTranslatef(-40, -4, 0);
+		for(int i=0; i<10;i++){
+			glTranslatef(4, 0, 0);
+			if(kokoske2[i].alive)
+				drawChicken();
+			kokoske2[i].x=(cx_curr+4*i-19.6);
+			kokoske2[i].y=cy_curr+16.5;
+		}
+		glTranslatef(-40, -4, 0);
+		for(int i=0; i<10;i++){
+			glTranslatef(4, 0, 0);
+			if(kokoske3[i].alive)
+				drawChicken();
+			kokoske3[i].x=(cx_curr+4*i-19.6);
+			kokoske3[i].y=cy_curr+12.5;
+		}
 	glPopMatrix();
 
-	if(lvl2){//dodaj uslov za helte
+	if(lvl2){
 		glPushMatrix();
 			glTranslatef(bcx_curr, -bcy_curr, 0);
 			drawBossChicken();
@@ -298,16 +333,14 @@ static void on_display(void){
 		//drawBossChickenColider();
 		//drawBossChickenEggColider();
 	}
-
-	
+	//drawChickenEggColider();
 	//drawChickenColider();
-
-	
 	glutSwapBuffers();
 }
 
 /*avion*/
 void drawPlane(){
+
 	glPushMatrix();
 		/*prednje krilo*/
 		glMaterialfv(GL_FRONT, GL_AMBIENT, ambient_tp);
@@ -337,13 +370,15 @@ void drawPlane(){
 		glMaterialfv(GL_FRONT, GL_AMBIENT, ambient_tp);
 		glMaterialfv(GL_FRONT, GL_DIFFUSE, difuz_tp);
 		glColor3f(0.7, 0.7, 0.8);
-		glScalef(0.5, 1, 3); /*1 = 15 iz prethodnog*/
+		glScalef(0.5, 1, 3);
 		glTranslatef(0, -1, 0);
 		glutSolidCube(1);
 	glPopMatrix();
 }
 
+/*avion - kolajder*/
 void drawPlaneColider(){
+
 	glColor3f(0, 0.5, 0.5);
 	glBegin(GL_POLYGON);
 		glVertex3f(x_curr - 1.5, +y_curr-10, 0);//gornja leva
@@ -355,6 +390,7 @@ void drawPlaneColider(){
 
 /*metak*/
 void drawBullet(){
+
 	for (int i = 0; i < MAX_BULLET_NUMBER; i++)
         	if (bullet_ypos[i] >= 0){
 		glPushMatrix();
@@ -368,7 +404,10 @@ void drawBullet(){
 		glPopMatrix();
 		}	
 }
+
+/*metak - kolajder*/
 void drawBulletColider(){
+
 	glColor3f(0, 0.5, 0.5);
 	for(int i=0; i<MAX_BULLET_NUMBER;i++){
 	glBegin(GL_POLYGON);
@@ -380,20 +419,9 @@ void drawBulletColider(){
 	}
 }
 
-/*kokoska kolajder*/
-void drawChickenColider(){
-	glColor3f(0, 0.5, 0.5);
-	for(int i=0; i<10;i++){
-	glBegin(GL_POLYGON);
-		glVertex3f(kokoske1[i].x,kokoske1[i].y, 0);//gornja leva
-		glVertex3f(kokoske1[i].x + 3,kokoske1[i].y, 0);
-		glVertex3f(kokoske1[i].x + 3,kokoske1[i].y - 2, 0);//donja desna
-		glVertex3f(kokoske1[i].x,kokoske1[i].y - 2, 0);
-	glEnd();
-	}
-}
 /*kokoska*/
 void drawChicken(){
+
 	glPushMatrix();
 		/*telo*/
 		glMaterialfv(GL_FRONT, GL_AMBIENT, ambient_c);
@@ -445,7 +473,53 @@ void drawChicken(){
 	glPopMatrix();
 }
 
+/*kokoske - kolajder*/
+void drawChickenColider(){
+
+	glColor3f(0, 0.5, 0.5);
+	for(int i=0; i<10;i++){
+		glBegin(GL_POLYGON);
+			glVertex3f(kokoske1[i].x,kokoske1[i].y, 0);//gornja leva
+			glVertex3f(kokoske1[i].x + 3,kokoske1[i].y, 0);
+			glVertex3f(kokoske1[i].x + 3,kokoske1[i].y - 2, 0);//donja desna
+			glVertex3f(kokoske1[i].x,kokoske1[i].y - 2, 0);
+		glEnd();
+	}
+}
+
+/*jaja kokosaka*/
+void drawChickenEgg(){
+
+	glPushMatrix();
+		glMaterialfv(GL_FRONT, GL_AMBIENT, ambient_z);
+		glMaterialfv(GL_FRONT, GL_DIFFUSE, difuz_z);
+		glColor3f(0.7, 0.7, 0.8);
+		glScalef(0.06,0.1,0.02);
+		glTranslatef(0, -20, 0);//zakomentarisi kad odradis koliziju
+		glutSolidSphere(7, 10, 10);
+	glPopMatrix();
+}
+
+/*jaja kokosaka- kolajder*/
+void drawChickenEggColider(){
+
+	glColor3f(0, 0.5, 0.5);
+	for(int i=0; i<10;i++){
+		if(jaje1[i].go && kokoske1[i].alive){
+			glBegin(GL_POLYGON);
+				printf("%lf\n", cx_curr);
+				glVertex3f(jaje1[i].x, jaje1[i].y, 0);//gornja leva
+				glVertex3f(jaje1[i].x + 1 , jaje1[i].y, 0);
+				glVertex3f(jaje1[i].x + 1, jaje1[i].y - 1, 0);//donja desna
+				glVertex3f(jaje1[i].x, jaje1[i].y - 1, 0);
+			glEnd();
+		}	
+	}
+}
+
+/*boss kokoska iz lvl2*/
 void drawBossChicken(){
+
 	glPushMatrix();
 		/*telo*/
 		glMaterialfv(GL_FRONT, GL_AMBIENT, ambient_c);
@@ -498,7 +572,9 @@ void drawBossChicken(){
 	glPopMatrix();
 }
 
+/*boss kokoska iz lvl2 - kolajder*/
 void drawBossChickenColider(){
+
 	glColor3f(0, 0.5, 0.5);
 	glBegin(GL_POLYGON);
 		glVertex3f(bcx_curr - 8, -bcy_curr+33, 0);//gornja leva
@@ -508,18 +584,22 @@ void drawBossChickenColider(){
 	glEnd();
 }
 
+/*jaje boss kokoske iz lvl2*/
 void drawBossChickenEgg(){
+
 	glPushMatrix();
-			glMaterialfv(GL_FRONT, GL_AMBIENT, ambient_c);
-			glMaterialfv(GL_FRONT, GL_DIFFUSE, difuz_c);
-			glColor3f(0.7, 0.7, 0.8);
-			glScalef(0.14,0.4,0.1);
-			glTranslatef(0,75,0);
-			glutSolidSphere(7, 10, 10);
+		glMaterialfv(GL_FRONT, GL_AMBIENT, ambient_c);
+		glMaterialfv(GL_FRONT, GL_DIFFUSE, difuz_c);
+		glColor3f(0.7, 0.7, 0.8);
+		glScalef(0.14,0.4,0.1);
+		glTranslatef(0,75,0);
+		glutSolidSphere(7, 10, 10);
 	glPopMatrix();
 }
 
+/*jaje boss kokoske iz lvl2 - kolajder*/
 void drawBossChickenEggColider(){
+
 	glColor3f(0, 0.5, 0.5);
 	glBegin(GL_POLYGON);
 		glVertex3f(bcx_curr - 1, -bey_curr-bcy_curr+33, 0);//gornja leva
@@ -528,8 +608,11 @@ void drawBossChickenEggColider(){
 		glVertex3f(bcx_curr - 1,-bey_curr-bcy_curr + 27, 0);
 	glEnd();
 }
+
+/*kretanje aviona*/
 static void on_timer1(int value){
-    /*proverava se da li poziv dolazi od odgovarajuceg tajmera*/
+
+   	/*proverava se da li poziv dolazi od odgovarajuceg tajmera*/
 	if (value == TIMER_ID1){
 		if(napred==1){
 		y_curr += SPEED;
@@ -566,16 +649,19 @@ static void on_timer1(int value){
 	}
 }
 
+
+/*kretanje kokoske iz nivoa 1 i 2 + kretanje jajeta iz nivoa 1*/
 static void on_timer2(int value){
-    /*proverava se da li poziv dolazi od odgovarajuceg tajmera*/
+
 	if (value != TIMER_ID2)
 		return;
 
+	//kretanje kokoske pocetak
 	cx_curr += v_x;
 
 	if (cx_curr <= LEVO_MAX_K){ /*leva granica*/
 		cy_curr=cy_curr-0.1; 
-		v_x *= -1; /*menjamo smer kretanja*/
+		v_x *= -1;
 	}
 	else if(cx_curr <= LEVO_MAX_K+1){
 		cy_curr=cy_curr-0.1;
@@ -590,25 +676,37 @@ static void on_timer2(int value){
 
 	napred=0;
 	levo=0;
+	//kretanje kokoske kraj
 
-	if(numOfDeathChickens!=10)
+	//jaje kokoske pocetak
+	if(eggy_curr<EGG_MAX_Y)
+		eggy_curr +=1;
+	else{
+		eggy_curr =0;
+		eggx_curr = cx_curr;
+	}
+	//jaje kokoske kraj
+
+	if(numOfDeathChickens!=30){
 		kolizija_avion_kokoska();
+		kolizija_jaje_avion();
+	}
+	else
+		lvl2=true;
 
 	if(lvl2){
-		if(bcy_curr<20){
+		if(bcy_curr<20)//ako se velika kokoska nije pozicionirala na sredinu ekrana
 			bcy_curr += 0.1;
-		}
 		else{//velika kokoska se zaustavila (implementiramo kretanje jajeta)
 			bey_curr += 1;
 
-			int r=rand()%100 + 20;//random vreme ispaljivanja jaja
+			int r=rand()%100 + 20;//random vreme ispaljivanja jajeta(duzina puta van ekrana)
 			if(bey_curr>r)
 				bey_curr=0;
+
 			kolizija_avion_boss_jaja();
+			kolizija_avion_boss_kokoska();
 		}
-		//printf("%f\n", bcy_curr);
-	
-		kolizija_avion_boss_kokoska();
 	}
 		
 	glutPostRedisplay();
@@ -616,11 +714,12 @@ static void on_timer2(int value){
 		glutTimerFunc(TIMER_INTERVAL2, on_timer2, TIMER_ID2);
 }
 
+/*kretanje metka*/
 static void on_timer3(int value){
+
 	if (value != TIMER_ID3)
 		return;
-	/*if(fire)
-		bullety_curr+=BULLET_SPEED;*/
+
         for (int i = 0; i < MAX_BULLET_NUMBER; i++)
         	if (bullet_ypos[i] >= 0) {
         		bullet_ypos[i] += BULLET_SPEED;
@@ -634,6 +733,7 @@ static void on_timer3(int value){
 }
 
 void inicijalizacija_osvetljenja(){
+
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
 
@@ -652,29 +752,45 @@ void inicijalizacija_osvetljenja(){
 }
 
 void kolizija_avion_kokoska(){
+
 	for(int i=0;i<10;i++){
-			//printf("%d. kokoska: %lf %lf, %s\n",i, kokoske1[i].x, kokoske1[i].y, kokoske1[i].alive? "yes" : "no");
+		//printf("%d. kokoska: %lf %lf, %s\n",i, kokoske1[i].x, kokoske1[i].y, kokoske1[i].alive? "yes" : "no");
 		for(int m=0;m<MAX_BULLET_NUMBER;m++){
 			//printf("%d. metak: %lf %lf\n",m, x_curr, bullet_ypos[m]);
 			if(kokoske1[i].y+5<=bullet_ypos[m]/2.5 && kokoske1[i].x<=x_curr && kokoske1[i].x + 3 >=x_curr
-			&& kokoske1[i].y+8>=bullet_ypos[m]/2.5){
+				&& kokoske1[i].y+8>=bullet_ypos[m]/2.5){
 				if(kokoske1[i].alive){
 					kokoske1[i].alive=false;
 					bullet_ypos[m] = -1;
 					numOfDeathChickens++;
-					if(numOfDeathChickens==10)
-						lvl2=true;
 					//printf("%d\n", numOfDeathChickens);
 				}
 				//printf("%d\n", i);
 				//animation_ongoing = 0;
 				//printf("%d %s\n", i, kokoske1[i].alive ? "true":"false");
 			}
+			else if(kokoske2[i].y+5<=bullet_ypos[m]/2.5 && kokoske2[i].x<=x_curr && kokoske2[i].x + 3 >=x_curr
+				&& kokoske2[i].y+8>=bullet_ypos[m]/2.5){
+				if(kokoske2[i].alive){
+					kokoske2[i].alive=false;
+					bullet_ypos[m] = -1;
+					numOfDeathChickens++;
+				}
+			}
+			else if(kokoske3[i].y+5<=bullet_ypos[m]/2.5 && kokoske3[i].x<=x_curr && kokoske3[i].x + 3 >=x_curr
+				&& kokoske3[i].y+8>=bullet_ypos[m]/2.5){
+				if(kokoske3[i].alive){
+					kokoske3[i].alive=false;
+					bullet_ypos[m] = -1;
+					numOfDeathChickens++;
+				}
+			}
 		}
 	}
 }
 
 void kolizija_avion_boss_kokoska(){
+
 	for(int i=0;i<MAX_BULLET_NUMBER;i++){
 		if(x_curr>=(bcx_curr - 7) && x_curr <= (bcx_curr+7) && bullet_ypos[i]/2.5>=32-bcy_curr
 		&& bullet_ypos[i]/2.5<=39-bcy_curr){
@@ -687,9 +803,24 @@ void kolizija_avion_boss_kokoska(){
 }
 
 void kolizija_avion_boss_jaja(){
+
 	if(((bcx_curr - 1) < (x_curr + 1.5)) && ((bcx_curr + 1) >= (x_curr - 1.5)) && 
-		((27-bey_curr-bcy_curr) < (y_curr - 11)) ){
+		((27-bey_curr-bcy_curr) < (y_curr - 11))){
 		sleep(1);
 		init();
+	}
+}
+
+void kolizija_jaje_avion(){
+
+	for(int i=0;i<10;i++){
+		if(kokoske1[i].alive && jaje1[i].go){
+			if(jaje1[i].x + 0.5 >= x_curr -1.5 && jaje1[i].x + 0.5 <= x_curr +1.5 && 
+				jaje1[i].y-1 <= y_curr-10){
+					init();
+					sleep(1);
+
+			}
+		}
 	}
 }
